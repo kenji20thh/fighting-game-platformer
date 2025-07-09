20f9973eeafa467f61c3fc5a8d3ac8b82e658c84
kenji20thh <amineelayachi22@outlook.com>
2025-07-09 17:56:44 +0200

    draw-player-rec

--- index.js
+++ index.js
@@ -10,9 +10,15 @@ class Sprite {
     constructor(position) {
         this.position = position 
     }
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 50, 150)
+    }
 }
 
 const player = new Sprite({
     x:0,
     y:0
 })
+
+player.draw()
