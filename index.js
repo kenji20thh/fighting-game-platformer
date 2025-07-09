8d70a32b4231e3f8352dfb7605641c9dea98e750
kenji20thh <amineelayachi22@outlook.com>
2025-07-09 19:06:06 +0200

    falling-and-stop

--- index.js
+++ index.js
@@ -10,14 +10,19 @@ class Sprite {
     constructor({ position, velocity }) {
         this.position = position
         this.velocity = velocity
+        this.height = 150
+        this.width = 50
     }
     draw() {
         c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, 150)
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
     update() {
         this.draw()
-        this.position.y += 10    
+        this.position.y += this.velocity.y
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0
+        }  
     }
 }
 
@@ -28,7 +33,7 @@ const player = new Sprite({
     },
     velocity: {
         x: 0,
-        y: 0
+        y: 10
     }
 })
 
@@ -40,13 +45,14 @@ const enemy = new Sprite({
     },
     velocity: {
         x: 0,
-        y: 0
+        y: 10
     }
 })
 
 
 const animate = () => {
     window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
