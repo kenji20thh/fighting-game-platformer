32a35ac0f5daa068eb596be20b9c1fda3e6b04b7
kenji20thh <amineelayachi22@outlook.com>
2025-07-10 19:18:37 +0200

    box-position-object

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ class Sprite {
         this.width = 50
         this.lastKey
         this.attackBox = {
-            position: this.position,
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
             width: 100,
             height: 50
         }
@@ -28,18 +31,23 @@ class Sprite {
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
 
         //attack box
-        if (this.isAttacking) {
-            c.fillStyle = 'green'
-            c.fillRect(this.attackBox.position.x,
-                this.attackBox.position.y,
-                this.attackBox.width,
-                this.attackBox.height)
-        }
+        // if (this.isAttacking) {
+        c.fillStyle = 'green'
+        c.fillRect(this.attackBox.position.x,
+            this.attackBox.position.y,
+            this.attackBox.width,
+            this.attackBox.height)
+        // }
     }
     update() {
         this.draw()
+
+        this.attackBox.position.x = this.position.x
+        this.attackBox.position.y = this.position.y
+
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
+
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
             this.velocity.y = 0
         } else {
